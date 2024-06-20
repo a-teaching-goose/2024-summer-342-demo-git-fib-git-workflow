a36708bf89c5404dcfeaa6b447c88e70904045a6
Peng Du <rick.peng.du@gmail.com>
2024-06-20 10:11:52 -0700

    code from the teacher

---
+++ src/fibonacci_sequence/fib.cpp
@@ -0,0 +1,7 @@
+#include "fib.hpp"
+
+uint fib(const uint n) {
+    // TODO: add function that generate the nth fibonacci sequence number
+    //  For example: with 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, fib(3) should return 3.
+    return -1;  // TODO: this is a placeholder. Replace with actual code.
+}
---
+++ test/unit_test_fib.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "fibonacci_sequence/fib.hpp"
+
+TEST(fib, test) {
+    ASSERT_TRUE(false);
+}
